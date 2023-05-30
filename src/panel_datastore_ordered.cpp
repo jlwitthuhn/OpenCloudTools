4adad629c47ae077e147c02202e3932be96df04e
Jeff <>
2023-05-29 18:43:07 -0700

    Add non-functional 'find' buttons to OrderedDatastorePanel

--- src/panel_datastore_ordered.cpp
+++ src/panel_datastore_ordered.cpp
@@ -1,13 +1,38 @@
 #include "panel_datastore_ordered.h"
 
+#include <QBoxLayout>
 #include <QLineEdit>
 #include <QListWidget>
+#include <QPushButton>
 
 #include "profile.h"
 
 OrderedDatastorePanel::OrderedDatastorePanel(QWidget* parent, const QString& api_key) : BaseDatastorePanel(parent, api_key)
 {
+	// Main panel
+	{
+		find_ascending_button = new QPushButton{ "Find all (ascending)", search_submit_widget };
+		connect(find_ascending_button, &QPushButton::clicked, this, &OrderedDatastorePanel::pressed_find_ascending);
+
+		find_descending_button = new QPushButton{ "Find all (descending)", search_submit_widget };
+		connect(find_descending_button, &QPushButton::clicked, this, &OrderedDatastorePanel::pressed_find_descending);
+
+		QBoxLayout* const search_submit_layout = dynamic_cast<QBoxLayout*>(search_submit_widget->layout());
+		search_submit_layout->insertWidget(0, find_ascending_button);
+		search_submit_layout->insertWidget(1, find_descending_button);
+	}
+
 	clear_model();
+	handle_search_text_changed();
+}
+
+void OrderedDatastorePanel::handle_search_text_changed()
+{
+	BaseDatastorePanel::handle_search_text_changed();
+	const UniverseProfile* const selected_universe = UserProfile::get_selected_universe();
+	const bool enabled = search_datastore_name_edit->text().size() > 0 && selected_universe;
+	find_ascending_button->setEnabled(enabled);
+	find_descending_button->setEnabled(enabled);
 }
 
 void OrderedDatastorePanel::clear_model()
@@ -27,4 +52,14 @@ void OrderedDatastorePanel::refresh_datastore_list()
 			this_item->setHidden(!matches_filter);
 		}
 	}
+}
+
+void OrderedDatastorePanel::pressed_find_ascending()
+{
+
+}
+
+void OrderedDatastorePanel::pressed_find_descending()
+{
+
 }
--- src/panel_datastore_ordered.h
+++ src/panel_datastore_ordered.h
@@ -4,6 +4,8 @@
 
 #include "panel_datastore_base.h"
 
+class QPushButton;
+
 class OrderedDatastorePanel : public BaseDatastorePanel
 {
 	Q_OBJECT
@@ -11,6 +13,14 @@ public:
 	OrderedDatastorePanel(QWidget* parent, const QString& api_key);
 
 private:
+	virtual void handle_search_text_changed() override;
+
 	virtual void clear_model() override;
 	virtual void refresh_datastore_list() override;
+
+	void pressed_find_ascending();
+	void pressed_find_descending();
+
+	QPushButton* find_ascending_button = nullptr;
+	QPushButton* find_descending_button = nullptr;
 };
