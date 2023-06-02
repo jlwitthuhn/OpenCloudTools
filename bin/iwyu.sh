04f7370cef2d1efbafd08779c815141058be4c9f
Jeff <>
2023-06-02 00:46:45 -0700

    Fix iwyu script and update path for Fedora 38

--- bin/iwyu.sh
+++ bin/iwyu.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 qt_include_base="/usr/include/qt6"
-clang_include_dir="/usr/lib64/clang/14/include"
+clang_include_dir="/usr/lib64/clang/16/include"
 
 qt_defines="-DQT_CORE_LIB -DQT_GUI_LIB -DQT_NETWORK_LIB -DQT_NO_DEBUG -DQT_WIDGETS_LIB"
 local_includes="-I./extern/sqlite"
 qt_includes="-isystem ${qt_include_base} -isystem ${qt_include_base}/QtCore -isystem ${qt_include_base}/QtGui -isystem ${qt_include_base}/QtNetwork -isystem ${qt_include_base}/QtWidgets"
 cxxflags="-isystem $clang_include_dir -std=gnu++17 -c"
 
-flags="$qt_include_base $qt_defines $local_includes $qt_includes $cxxflags"
+flags="$qt_defines $local_includes $qt_includes $cxxflags"
 
 for filename in ./src/*.cpp; do
 	echo "Checking $filename"
