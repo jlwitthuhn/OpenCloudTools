752d40b7372775743a5a75ce9d088a7c85beba7e
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2024-08-21 02:22:24 -0700

    Add testing lua script for ordered datastores

---
+++ extra/roblox_test/setup_datastore_ordered.lua
@@ -0,0 +1,11 @@
+local DataStoreService = game:GetService("DataStoreService")
+
+local TestStore = DataStoreService:GetOrderedDataStore("TestStore")
+
+for i=0,500 do
+	print(i)
+    pcall(function()
+        TestStore:SetAsync("Test" .. string.format("%03d", i), i)
+    end)
+	wait(1.5)
+end
