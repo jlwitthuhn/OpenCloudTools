786787502a6352ea26299f35747b97ababc7f139
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2025-01-18 00:09:45 -0800

    Add git tag to assert dialog

--- src/assert.cpp
+++ src/assert.cpp
@@ -1,5 +1,6 @@
 #include "assert.h"
 
+#include <sstream>
 #include <string>
 
 #include <QMessageBox>
@@ -9,7 +10,11 @@ void oct_do_assert(const char* const file, int line, const bool condition)
 {
 	if (!condition)
 	{
-		const std::string message = std::string{ "Assert failed:\n" } + file + "\nLine " + std::to_string(line);
-		QMessageBox::critical(nullptr, "Assert Failed", QString::fromStdString(message));
+		std::stringstream stream;
+#ifdef GIT_DESCRIBE
+		stream << "Git: " << GIT_DESCRIBE << "\n";
+#endif
+		stream << "Assert failed:\n" << file << "\nLine " << std::to_string(line);
+		QMessageBox::critical(nullptr, "Assert Failed", QString::fromStdString(stream.str()));
 	}
 }
